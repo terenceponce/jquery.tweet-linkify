cdeb144902c8de8b1e09f067b85fa5070cda4f9b
Terence Ponce <terenceponce@gmail.com>
2012-09-07 10:48:27 +0800

    Add target, class, rel options for mentions and hashtags

--- jquery.tweet-linkify.js
+++ jquery.tweet-linkify.js
@@ -25,30 +25,43 @@
       excludeHyperlinks: false,
       excludeMentions: false,
       excludeHashtags: false,
-      target: '',
-      className: '',
-      rel: ''
+      hyperlinkTarget: '',
+      mentionTarget: '',
+      hashtagTarget: '',
+      hyperlinkClass: '',
+      mentionClass: '',
+      hashtagClass: '',
+      hyperlinkRel: '',
+      mentionRel: '',
+      hashtagRel: ''
     };
 
     var options = $.extend(defaultAttributes, options);
 
-    targetString = (options.target != '') ? 'target="_' + options.target + '"' : '';
-    classString = (options.className != '') ? 'class="' + options.className + '"' : '';
-    relString = (options.rel != '') ? 'rel="' + options.rel + '"' : '';
-    options = $.extend(defaultAttributes, options);
+    var hyperlinkTarget = (options.hyperlinkTarget != '') ? 'target="_' + options.hyperlinkTarget + '"' : '';
+    var mentionTarget = (options.mentionTarget != '') ? 'target="_' + options.mentionTarget + '"' : '';
+    var hashtagTarget = (options.hashtagTarget != '') ? 'target="_' + options.hashtagTarget + '"' : '';
+    var hyperlinkClass = (options.hyperlinkClass != '') ? 'class="' + options.hyperlinkClass + '"' : '';
+    var mentionClass = (options.mentionClass != '') ? 'class="' + options.mentionClass + '"' : '';
+    var hashtagClass = (options.hashtagClass != '') ? 'class="' + options.hashtagClass + '"' : '';
+    var hyperlinkRel = (options.hyperlinkRel != '') ? 'rel="' + options.hyperlinkRel + '"' : '';
+    var mentionRel = (options.mentionRel != '') ? 'rel="' + options.mentionRel + '"' : '';
+    var hashtagRel = (options.hashtagRel != '') ? 'rel="' + options.hashtagRel + '"' : '';
+
     if (options.excludeHyperlinks != true) {
-      tweet = tweet.replace(/(https\:\/\/|http:\/\/)([www\.]?)([^\s|<]+)/gi, '<a href="$1$2$3" ' + targetString + ' ' + classString + ' ' + relString + '>$1$2$3</a>');
-      tweet = tweet.replace(/([^https\:\/\/]|[^http:\/\/]|^)(www)\.([^\s|<]+)/gi, '$1<a href="http://$2.$3" ' + targetString + ' ' + classString + ' ' + relString + '>$2.$3</a>');
+      tweet = tweet.replace(/(https\:\/\/|http:\/\/)([www\.]?)([^\s|<]+)/gi, '<a href="$1$2$3" ' + hyperlinkTarget + ' ' + hyperlinkClass + ' ' + hyperlinkRel + '>$1$2$3</a>');
+      tweet = tweet.replace(/([^https\:\/\/]|[^http:\/\/]|^)(www)\.([^\s|<]+)/gi, '$1<a href="http://$2.$3" ' + hyperlinkTarget + ' ' + hyperlinkClass + ' ' + hyperlinkRel + '>$2.$3</a>');
       tweet = tweet.replace(/<([^a]|^\/a])([^<>]+)>/g, "&lt;$1$2&gt;").replace(/&lt;\/a&gt;/g, "</a>").replace(/<(.)>/g, "&lt;$1&gt;").replace(/\n/g, '<br />');
     }
 
     if (options.excludeMentions != true) {
-      tweet = tweet.replace(/(@)(\w+)/, '<a href="http://twitter.com/$2">$1$2</a>');
+      tweet = tweet.replace(/(@)(\w+)/, '<a href="http://twitter.com/$2" ' + mentionTarget + ' ' + mentionClass + ' ' + mentionRel + '>$1$2</a>');
     }
 
     if (options.excludeHashtags != true) {
-      tweet = tweet.replace(/(#)(\w+)/, '<a href="https://twitter.com/search/?src=hash&q=%23$2">$1$2</a>');
+      tweet = tweet.replace(/(#)(\w+)/, '<a href="https://twitter.com/search/?src=hash&q=%23$2" ' + hashtagTarget + ' ' + hashtagClass + ' ' + hashtagRel + '>$1$2</a>');
     }
+    
     return tweet;
   }
     
